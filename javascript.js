d732dffcd34560958c0c4ee3c83e4d9fb269352d
arvindvs <33942304+arvindvs@users.noreply.github.com>
2018-02-03 05:22:46 -0800

    Added descriptions to the About Section and modified icons

--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-         $('menu').addClass('changeColor')
-      }
-      if ($(this).scrollTop() < 100) {
-         $('menu').removeClass('changeColor')
-      }
-   });
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('menu').outerHeight();
+
+var didScroll;
+
+// on scroll, let the interval function know the user has scrolled
+$(window).scroll(function(event){
+  didScroll = true;
 });
+
+// run hasScrolled() and reset didScroll status
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+	var st = $(this).scrollTop();
+	if (Math.abs(lastScrollTop—st) <= delta) return;
+	if (st > lastScrollTop && st > navbarHeight){
+		$('menu').removeClass('nav-down').addClass('nav-up');
+	} else {
+		if(st + $(window).height() < $(document).height()) {
+			$('menu').removeClass('nav-up').addClass('nav-down');
+		}
+	}
+	lastScrollTop = st;
+}
